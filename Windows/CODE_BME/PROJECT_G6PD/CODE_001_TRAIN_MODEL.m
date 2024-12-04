b41d54dcb8359caa6629c96f435c65d54579b6e1
KungLobster69 <k.lobster69@gmail.com>
2024-12-04 14:52:51 +0700

    update

--- Windows/CODE_BME/PROJECT_G6PD/CODE_001_TRAIN_MODEL.m
+++ Windows/CODE_BME/PROJECT_G6PD/CODE_001_TRAIN_MODEL.m
@@ -2,4 +2,15 @@ clc
 clear
 close all
 
-MEAN_YGH = load('C:\Users\BMEI CMU\Documents\G6PD\YGH.mat')
+load('C:\Users\BMEI CMU\Documents\G6PD\YGH.mat');
+
+for SIZE_CUL = 1:size(YGH_Color,2)
+    for SIZE_ROW = 1:size(YGH_Color,1)
+        YGH_NORMALIZE(SIZE_ROW,SIZE_CUL) = (YGH_Color(SIZE_ROW,SIZE_CUL)-mean(YGH_Color(:,SIZE_CUL)))/var(YGH_Color(:,SIZE_CUL));
+    end
+end
+
+ABNORMAL = YGH_NORMALIZE(1:11,:);
+INTERMEDIATE = YGH_NORMALIZE(12:17,:);
+NORMAL = YGH_NORMALIZE(17:59,:);
+
