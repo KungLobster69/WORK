68540657103ce452f5ce498cf521b6593c5ce66c
KungLobster69 <96777955+KungLobster69@users.noreply.github.com>
2025-04-29 12:17:52 +0700

    Create run_sgfcmed.bat

---
+++ Windows/CODE_BME/PROJECT_CYBER_SECURITY/run_sgfcmed.bat
@@ -0,0 +1,10 @@
+
+@echo off
+echo ============================================
+echo Running 001_sgFCMed_Full.py ...
+echo ============================================
+cd /d C:\Users\BMEi\Documents\GitHub\WORK\Windows\CODE_BME\PROJECT_CYBER_SECURITY\CODE_02
+python 001_sgFCMed_Full.py
+echo ============================================
+echo Finished running 001_sgFCMed_Full.py
+pause
