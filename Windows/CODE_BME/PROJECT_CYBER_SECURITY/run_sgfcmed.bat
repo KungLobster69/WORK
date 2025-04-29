544429c43200d55b3232b1cb59f755fd1e80bb89
KungLobster69 <96777955+KungLobster69@users.noreply.github.com>
2025-04-29 16:24:37 +0700

    Update run_sgfcmed.bat

--- Windows/CODE_BME/PROJECT_CYBER_SECURITY/run_sgfcmed.bat
+++ Windows/CODE_BME/PROJECT_CYBER_SECURITY/run_sgfcmed.bat
@@ -1,17 +1,17 @@
 @echo off
 echo ============================================
-echo Running 001_sgFCMed_Full_benign.py ...
+echo Running 001_sgFCMed_Full_benign.py in a new window...
 echo ============================================
 cd /d C:\Users\BMEi\Documents\GitHub\WORK\Windows\CODE_BME\PROJECT_CYBER_SECURITY
-start "" python 001_sgFCMed_Full_benign.py
+start "Benign Window" cmd /k "python 001_sgFCMed_Full_benign.py"
 
 echo ============================================
-echo Running 001_sgFCMed_Full_malware.py ...
+echo Running 001_sgFCMed_Full_malware.py in a new window...
 echo ============================================
-start "" python 001_sgFCMed_Full_malware.py
+start "Malware Window" cmd /k "python 001_sgFCMed_Full_malware.py"
 
 echo ============================================
-echo Both scripts are running in parallel ...
+echo Both scripts are running in separate windows...
 echo ============================================
 
 pause
