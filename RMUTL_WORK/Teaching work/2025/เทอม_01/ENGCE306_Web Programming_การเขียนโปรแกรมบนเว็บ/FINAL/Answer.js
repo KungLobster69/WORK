798b96ffc883c0a0f26dcd3a25b043d6790df140
KungLobster69 <k.lobster69@gmail.com>
2025-09-30 13:35:57 +0700

    update

---
+++ RMUTL_WORK/Teaching work/2025/เทอม_01/ENGCE306_Web Programming_การเขียนโปรแกรมบนเว็บ/FINAL/Answer.js
@@ -0,0 +1,18 @@
+// 04
+const newUser: User = { // การระบุ Type ที่ตัวแปรจะช่วยให้ TS ตรวจสอบได้ทันที
+  userId: 1, // เปลี่ยนจาก String 'U001' เป็น Number
+  username: 'testuser',
+  email: 'test@example.com',
+  isActive: true // เปลี่ยนจาก String 'true' เป็น Boolean true
+};
+
+// 09
+{
+  "_id": "6515c1d683b511e673424d52",
+  "productName": "Mouse ไร้สาย",
+  "price": 990,
+  "tags": ["computer", "gadget", "wireless"],
+  "onSale": true,
+  "createdAt": "2025-09-28T16:02:30.987Z",
+  "updatedAt": "2025-09-28T16:02:30.987Z"
+}
