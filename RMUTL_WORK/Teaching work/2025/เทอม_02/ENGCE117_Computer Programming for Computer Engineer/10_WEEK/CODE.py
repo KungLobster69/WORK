41301a215ddc3ce6c3eda23cb7f5c0e09626117c
KungLobster69 <k.lobster69@gmail.com>
2026-02-24 15:54:58 +0700

    update

--- RMUTL_WORK/Teaching work/2025/เทอม_02/ENGCE117_Computer Programming for Computer Engineer/10_WEEK/CODE.py
+++ RMUTL_WORK/Teaching work/2025/เทอม_02/ENGCE117_Computer Programming for Computer Engineer/10_WEEK/CODE.py
@@ -1,25 +1,10 @@
 import tkinter as tk
 
-class MyApp(tk.Tk):
-    def __init__(self):
-        super().__init__() # สร้างหน้าต่างหลัก
-        self.title("My OOP GUI")
-        self.geometry("300x200")
-        
-        # วางโครงสร้าง UI
-        self.create_widgets()
+def on_click():
+    print("Button Clicked!")
 
-    def create_widgets(self):
-        # สร้าง Widget และเก็บไว้ใน self
-        self.label = tk.Label(self, text="Hello OOP GUI!", font=("Arial", 14))
-        self.label.pack(pady=20)
-        
-        self.btn = tk.Button(self, text="Click Me", command=self.on_click)
-        self.btn.pack(pady=10)
-        
-    def on_click(self):
-        self.label.config(text="Button Clicked!", fg="red")
+root = tk.Tk()
+btn = tk.Button(root, text="Click Me", command=on_click)
+btn.pack()
 
-if __name__ == "__main__":
-    app = MyApp()
-    app.mainloop()
+root.mainloop()
